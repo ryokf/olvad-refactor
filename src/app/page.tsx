e350be7a4b8c8d9168c668f264e9f8d36efec170
ryokf <ryokhrisnaf@gmail.com>
2025-02-06 10:44:28 +0700

    Add Contact component and integrate it into the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/Home/About";
+import Contact from "@/components/Home/Contact";
 import Hero from "@/components/Home/Hero";
 import Order from "@/components/Home/Order";
 import Reviews from "@/components/Home/Reviews";
@@ -10,6 +11,7 @@ export default function Home() {
       <Order></Order>
       <About></About>
       <Reviews></Reviews>
+      <Contact></Contact>
     </>
   );
 }
