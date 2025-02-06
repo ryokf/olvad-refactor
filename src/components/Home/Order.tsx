3f934e11950437ecd5491864269504f4abfa15ce
ryokf <ryokhrisnaf@gmail.com>
2025-02-06 12:06:03 +0700

    Update Order component with promotional messaging for Ramadhan discounts

--- src/components/Home/Order.tsx
+++ src/components/Home/Order.tsx
@@ -7,8 +7,8 @@ const Order = () => {
     return (
         <div className="grid grid-cols-1 w-10/12 mx-auto min-h-screen">
             <div className="">
-                <h1 className='text-3xl font-bold text-tertiary my-2 mx-auto'>We serve and deliver delectable food</h1>
-                <p className='text-secondary text-sm font-semibold'>Order from the comfort of your home and have it delivered to your doorstep.</p>
+                <h1 className='text-3xl font-bold text-tertiary my-2 mx-auto'>Spesial Ramadhan! Nikmati Diskon Hingga 50%</h1>
+                <p className='text-secondary text-sm font-semibold'>Sambut bulan suci dengan kelezatan roti segar dari kami! Dapatkan potongan harga spesial untuk semua varian roti dan kue favorit Anda.</p>
                 <p className='font-semibold text-3xl text-center text-red-500 mt-6'>4 : 10 : 10 : 9</p>
                 <p className='text-center text-red-500 font-semibold mb-2'>Hurry to take of the offer!!!</p>
                 <Button theme={buttonTheme} color='failure' className='!w-full mt-4 font-bold'>Order Now!!!</Button>
