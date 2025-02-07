067d8982cbb8be10cd224fca9fd377835ea5a91b
ryokf <ryokhrisnaf@gmail.com>
2025-02-07 11:00:03 +0700

    Refactor Card component to accept product props and update Order component to display products dynamically in a carousel

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,25 +4,33 @@
 import cardTheme from "@/themes/card";
 import { Badge, Card } from "flowbite-react";
 
-const CardComponent = () => {
+interface Product {
+    id: number;
+    name: string;
+    category: number;
+    price: number;
+    description: string;
+    photo: string;
+}
+const CardComponent = ({id, name = "", price = 0, description = "", photo = "https://flowbite.com/docs/images/products/apple-watch.png", category = 0} : Product) => {
     return (
         <Card
             theme={cardTheme}
-            className="max-w-80"
+            className="max-w-80 mx-auto"
             imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-            imgSrc="https://flowbite.com/docs/images/products/apple-watch.png"
+            imgSrc={photo}
         >
-            <Badge className="w-fit bg-tertiary text-white" color="success">Category</Badge>
+            <Badge className="w-fit bg-tertiary text-white" color="success">{category}</Badge>
 
             <h5 className="font-semibold tracking-tight text-primary dark:text-white">
-                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                {name}
             </h5>
 
             <div className="mb-2 flex items-center">
-                <p className="text-sm font-light text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam facere sint fugiat vitae vel quam</p>
+                <p className="text-sm font-light text-secondary">{description}</p>
             </div>
             <div className="flex items-center justify-between">
-                <span className="text-2xl font-bold text-primary dark:text-white">$599</span>
+                <span className="text-2xl font-bold text-primary dark:text-white">{price}</span>
                 <button
                     className="rounded-lg hover:bg-tertiary px-5 py-2.5 text-center text-sm font-medium text-white bg-primary focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
                 >
--- src/components/Home/Order.tsx
+++ src/components/Home/Order.tsx
@@ -5,16 +5,18 @@ import React, { useEffect, useState } from 'react'
 import CardComponent from '../Card';
 import buttonTheme from '@/themes/button';
 import { supabase } from '@/config/db';
+import carouselTheme from '@/themes/caraousel';
+
+interface Product {
+    id: number;
+    name: string;
+    category: number;
+    price: number;
+    description: string;
+    photo: string;
+}
 
 const Order = () => {
-    interface Product {
-        id: number;
-        name: string;
-        category_id: number;
-        price: number;
-        description: string;
-        photo: string;
-    }
 
     const [products, setProducts] = useState<Product[] | null>([]);
 
@@ -30,20 +32,22 @@ const Order = () => {
     console.log(products)
     
     return (
-        <div className="grid grid-cols-1 w-10/12 mx-auto min-h-screen">
-            <div className="">
+        <div className="min-h-screen">
+            <div className="w-10/12 mx-auto">
                 <h1 className='text-3xl font-bold text-tertiary my-2 mx-auto'>Spesial Ramadhan! Nikmati Diskon Hingga 50%</h1>
                 <p className='text-secondary text-sm font-semibold'>Sambut bulan suci dengan kelezatan roti segar dari kami! Dapatkan potongan harga spesial untuk semua varian roti dan kue favorit Anda.</p>
                 <p className='font-semibold text-3xl text-center text-red-500 mt-6'>4 : 10 : 10 : 9</p>
                 <p className='text-center text-red-500 font-semibold mb-2'>Hurry to take of the offer!!!</p>
                 <Button theme={buttonTheme} color='failure' className='!w-full mt-4 font-bold'>Order Now!!!</Button>
             </div>
-            <Carousel className="w-full" indicators={false} pauseOnHover>
-                <CardComponent></CardComponent>
-                <CardComponent></CardComponent>
-                <CardComponent></CardComponent>
-                <CardComponent></CardComponent>
-                <CardComponent></CardComponent>
+            <Carousel theme={carouselTheme} className="w-full my-10" indicators={false} pauseOnHover>
+                {
+                    products?.map((product) => {
+                        return (
+                            <CardComponent key={product.id} {...product} />
+                        )
+                    })
+                }
             </Carousel>
         </div>
     )
